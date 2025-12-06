618731bfe8d9bf61d1e74541916032b5ee90c621
PuspharajSubramanian <praveenpushparaj02@gmail.com>
2025-12-06 21:25:10 +0530

    Add files via upload
    Day01

---
+++ week1/datatypes.js
@@ -0,0 +1,8 @@
+var broswerVersion=7.2
+var broswerName="Firefox"
+var isBroswerOpen= true
+var history=null
+console.log("Broswer Version is:"+broswerVersion+ " and datatype is:" +typeof(broswerVersion))
+console.log("Broswer Name is:"+broswerName+ " and datatype is:" +typeof(broswerName))
+console.log("Broswer is opened/visible:"+isBroswerOpen+ " and datatype is:" +typeof(isBroswerOpen))
+console.log("History:"+history+ " and datatype is:" +typeof(history))
---
+++ week1/variables.js
@@ -0,0 +1,11 @@
+var num=45
+var num="Hello"
+console.log("num:"+num+ " and datatype:"+typeof +num)
+
+let empname="Testing"
+empname=123
+console.log("empname:"+empname+ " and datatype:"+typeof +empname)
+
+const final=34
+//const final=45
+console.log("final:"+final+ " and datatype:"+typeof +final)
